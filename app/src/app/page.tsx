76280f5b024331254fc856c7468f4006fafeaddf
Teodor Tsachev <teo.tsachev@gmail.com>
2025-01-19 18:28:32 +0200

    Add a UserList client component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.scss";
+import UserList from "@/components/UserList/UserList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div>Boilerplate</div>
+        <UserList />
       </main>
     </div>
   );
