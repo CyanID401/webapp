166cac1de47eeae61f975c08cd75448cdca5f444
Teodor Tsachev <teo.tsachev@gmail.com>
2025-01-23 17:47:32 +0200

    Add the VPC module

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+output "aws_region" {
+  value       = var.aws_region
+  description = "The AWS region"
+}
+
+output "aws_account_id" {
+  value       = var.aws_account_id
+  description = "The AWS account ID"
+}
+
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The ID of the VPC"
+}
+
+output "vpc_public_subnets_ids" {
+  value       = module.vpc.public_subnets
+  description = "The IDs of the public subnets"
+}
+
+output "vpc_private_subnets_ids" {
+  value       = module.vpc.private_subnets
+  description = "The IDs of the private subnets"
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  default     = "eu-north-1"
+  description = "The AWS region"
+}
+
+
+variable "aws_account_id" {
+  default     = "307946645974"
+  description = "The AWS account ID"
+}
+
+variable "azs" {
+  type        = list(any)
+  default     = ["eu-north-1a"]
+  description = "List of Availability Zones"
+}
+
+variable "vpc_name" {
+  default     = "my-vpc"
+  description = "Name of the VPC"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "CIDR block of the VPC"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(string)
+  default     = ["10.0.9.0/24"]
+  description = "CIDR block for Private Subnet"
+}
